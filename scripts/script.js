f3609dd0f8547d33abf08526653f7d9570455ece
yelizerbas <94406320+yelizerbas@users.noreply.github.com>
2023-04-25 10:56:45 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,24 @@
 // JavaScript Document
 console.log("Howdy!");
+
+const tubesList = document.querySelector('main ol');
+const tubes = document.querySelectorAll('main ol li:not(:last-of-type) button');
+const bag = document.querySelector('main ol li:last-of-type');
+
+let hasSelected = false;
+
+function tubeClick (e) {
+    if (hasSelected == true) {
+        return;
+    }
+
+    hasSelected = true;
+    const listItem = e.currentTarget.parentElement;
+    tubesList.classList.add('locked');
+    listItem.classList.add('selected');
+    bag.classList.add('fixed');
+}
+
+tubes.forEach(tube => {
+    tube.addEventListener('click', tubeClick);
+})
