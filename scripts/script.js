4965b87c9d074a1a169116e3f4ae98192f3d2dc7
yelizerbas <94406320+yelizerbas@users.noreply.github.com>
2023-05-03 14:56:44 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ const bag = document.querySelector('main ol li:last-of-type');
 
 let hasSelected = false;
 
+// function zakje op zelfde plek laten staan
 function tubeClick (e) {
     if (hasSelected == true) {
         return;
@@ -22,3 +23,12 @@ function tubeClick (e) {
 tubes.forEach(tube => {
     tube.addEventListener('click', tubeClick);
 })
+
+
+// function alle m&ms oranje maken
+// https://www.youtube.com/watch?v=E6J2fosujWQ
+const oranjeButton = document.querySelector('main ol li:nth-of-type(3) button');
+
+oranjeButton.addEventListener('dblclick', () => {
+document.body.style.backgroundImage = "url('images/MMs-buis-oranje.svg')";
+});
